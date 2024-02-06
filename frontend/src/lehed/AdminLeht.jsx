8edd9f89db4e5bc5d85483b327d24bb245c897cb
Rbr4t <robertkoor0@gmail.com>
2024-02-06 16:59:34 +0200

    unustasin ühe faili

--- frontend/src/lehed/AdminLeht.jsx
+++ frontend/src/lehed/AdminLeht.jsx
@@ -1,9 +1,28 @@
 import Päis from "./komponendid/Päis"
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { GlobalStyles, CssBaseline, Container } from "@mui/material";
+
 export default function AdminLeht() {
+    const defaultTheme = createTheme();
+
     return (
         <>
+            <ThemeProvider theme={defaultTheme}>
+            <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+            <CssBaseline />
+            
             <Päis />
-            <h1>Test</h1>
+
+            <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
+                
+
+
+                
+                
+            </Container>
+            
+                
+            </ThemeProvider>
         </>
     )
 }
