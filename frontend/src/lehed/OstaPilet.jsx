7dbfc7597bb71395511d5107d719b0fbb4229af8
Rbr4t <robertkoor0@gmail.com>
2024-02-06 21:36:34 +0200

    piletite näitamise süsteem

--- frontend/src/lehed/OstaPilet.jsx
+++ frontend/src/lehed/OstaPilet.jsx
@@ -1,33 +1,91 @@
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import GlobalStyles from '@mui/material/GlobalStyles';
-import Container from '@mui/material/Container';
-import { Autocomplete } from '@mui/material';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { TextField, Container, GlobalStyles, CssBaseline, Button, Autocomplete, Grid } from '@mui/material';
+import { useState } from 'react';
 
+import Sõit from './komponendid/Sõit';
 import Päis from './komponendid/Päis';
 
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
+let algusKohad = ["Nõo", "Tartu"]
+let loppKohad = ["Nõo", "Tartu"]
+const peatused = [
+    {"peatused":["Tartu", "Teaduspark", "Nõo", "Elva"], "kuupäev": Date()},
+    {"peatused":["Elva", "Nõo", "Tartu"], "kuupäev": Date()}
+] 
+
 export default function OstaPilet() {
+  const [formAndmed, setFormAndmed] = useState({
+    algus: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+  });
+
+  const handleChange = (e) => {
+    setFormAndmed({
+      ...formAndmed,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can perform form submission logic here, such as sending data to a server
+    console.log(formAndmed);
+    // Clear form fields after submission
+    setFormAndmed({
+        algus: '',
+      lastName: '',
+      email: '',
+      phoneNumber: '',
+    });
+  };
+
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' }}} />
       <CssBaseline />
     
       <Päis />
 
-      <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
-        
+      <Container disableGutters maxWidth="md" component="main" sx={{ pt: 8, pb: 6 }} >
+    
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+                <Grid container direction="row" justifyContent="center" alignItems="center" gap={5} >
+                    <Autocomplete
+                        disablePortal
+                        id="algus"
+                        options={algusKohad}
+                        sx={{ width: 300 }}
+                        renderInput={(params) => <TextField {...params} label="Alguskoht" />}
+                    />
+                    <ArrowForwardIosIcon></ArrowForwardIosIcon>
+                    <Autocomplete
+                        disablePortal
+                        id="lopp"
+                        options={loppKohad}
+                        sx={{ width: 300 }}
+                        renderInput={(params) => <TextField {...params} label="Sihtkoht" />}
+                    />
+                    <Button
+                    variant="contained"
+                    color="primary"
 
+                    type="submit"
+                >
+                    Otsi
+                </Button>
+                </Grid>
+                            
+        </form>
 
-        
-        
+        <Container>
+            <Sõit liinid={peatused} kuupäev={Date()} />
+        </Container>
       </Container>
      
         
---
+++ frontend/src/lehed/komponendid/Sõit.jsx
@@ -0,0 +1,89 @@
+/* eslint-disable react/prop-types */
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Button, ListItem, ListItemText, List } from '@mui/material';
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function Sõit({liinid}) {
+  const [expanded, setExpanded] = React.useState([]);
+
+  const handleExpandClick = (index) => {
+    const newExpanded = [...expanded];
+    newExpanded[index] = !newExpanded[index];
+    setExpanded(newExpanded);
+  };
+
+  return (
+    <>
+    {liinid.map((liin, index) => (
+        <Card key={index}>
+            <CardHeader
+                
+                action={
+                <IconButton>
+                    <DirectionsBusIcon />
+                </IconButton>
+                }
+                title={liin.peatused[0] + " - " + liin.peatused[liin.peatused.length - 1]}
+                subheader={liin.kuupäev}
+            />
+            
+            
+            <CardActions disableSpacing>
+                <Button variant='contained'>
+                    Osta pilet 
+                </Button>
+                <ExpandMore
+                expand={expanded[index]}
+                onClick={() => handleExpandClick(index)}
+                aria-expanded={expanded[index]}
+                aria-label="show more"
+                >
+                <ExpandMoreIcon />
+                </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded[index]} timeout="auto" unmountOnExit>
+                <CardContent>
+                <Typography paragraph>Peatused</Typography>
+                <List >
+                    {liin.peatused.map((d, index) => (
+                        
+                        <ListItem key={index}>
+                            
+                            <ListItemText
+                            primary={`${d}`}
+                            secondary={'kellaaeg'}
+                            />
+                        </ListItem>
+                        
+                    ))}
+                </List>
+
+                    
+                
+                </CardContent>
+            </Collapse>
+        </Card>
+    ))}
+    </>
+  );
+}
